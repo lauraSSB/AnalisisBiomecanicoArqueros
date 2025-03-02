1b6f8dcd15357e30c90f8dd62032a229a549f4c7
LauraSSB <laurasalamanca1135@gmail.com>
2025-03-02 13:40:47 -0500

    Preprocesamiento saques de Piso

--- Codigo/preprocesamiento_videos.py
+++ Codigo/preprocesamiento_videos.py
@@ -1,1 +1,159 @@
-print("Hello world")
+import cv2
+import mediapipe as mp
+import numpy as np
+
+#Función que establece el parametro biomecanico del inicio del video
+#Recordar que el (0,0) esta en la esquina superior izquierda
+def inicio_video(landmarks,mp_pose):
+    rodilla = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y 
+    tobillo = landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y
+    if (rodilla>tobillo):
+        return True
+    else: 
+        return False
+
+#Función que establece el parametro biomecanico del fin del video
+#El bool del inicio debe ser true para que pueda encontrar un fin del video
+#El tobillo de la pierna de pateo debe ser mayor que el tobillo de la pierna de no pateo: 
+    #en laterales es mayor en la coordenada X
+    #en tasera es mayor en la coordenada Z
+def fin_video_coordenadas(camara,landmarks,mp_pose, inicio):
+    print(camara," ",inicio)
+    tobillo_pateo = landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x
+    rodilla_pateo = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x
+    cadera_pateo = landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x
+        
+    tobillo_no_pateo = landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x
+    rodilla_no_pateo = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x
+    cadera_no_pateo = landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x
+
+    if (camara == "T" and inicio == True):
+        tobillo_pateo = landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].z
+        rodilla_pateo = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].z
+        cadera_pateo = landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].z
+        
+        tobillo_no_pateo = landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].z
+        rodilla_no_pateo = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].z
+        cadera_no_pateo = landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].z
+
+        if(tobillo_pateo > tobillo_no_pateo and rodilla_pateo > rodilla_no_pateo):
+            return True
+        else:
+            return False
+
+    elif (camara == "LD" and inicio == True):
+        if(tobillo_pateo > tobillo_no_pateo and rodilla_pateo > rodilla_no_pateo):
+            return True
+        else:
+            return False
+    elif (camara == "LI" and inicio == True):
+        if(tobillo_pateo < tobillo_no_pateo and rodilla_pateo < rodilla_no_pateo):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def lectura_video(path):
+    print(path)
+    mp_marcar = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
+    captura = cv2.VideoCapture(path,cv2.CAP_FFMPEG)
+
+    if not captura.isOpened():
+        print("Error al abrir el video")
+        exit()
+
+    ancho = int(captura.get(cv2.CAP_PROP_FRAME_WIDTH))
+    alto = int(captura.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(captura.get(cv2.CAP_PROP_FPS))
+    total_frames = int(captura.get(cv2.CAP_PROP_FRAME_COUNT))
+    ret, frame = captura.read()
+    h, w, _ = frame.shape
+    scale_factor = 1
+    num_frame_inicio = 0
+
+    bandera_rodilla = False
+    bandera_fin = False
+    camara = ""
+    pausa = False  # Variable para pausar el video
+
+    print("Ancho:", ancho, "Alto:", alto, "FPS:", fps, "Total_frames:", total_frames)
+
+    with mp_pose.Pose(min_detection_confidence=0.90, min_tracking_confidence=0.9, model_complexity=2) as pose: 
+        while captura.isOpened():
+            if not pausa:  # Solo leer un nuevo frame si no está en pausa
+                ret, frame = captura.read()
+
+                if ret:
+                    if "trasera" in path.lower():
+                        camara = "T"
+                        frame = cv2.rotate(frame, cv2.ROTATE_180)  # Rota la imagen 180°
+                    elif "LD" in path:
+                        camara = "LD"
+                    else:
+                        camara = "LI"
+                
+                    resultados = pose.process(frame)
+
+                    frame.flags.writeable = True
+
+                    if resultados.pose_landmarks:
+                        landmarks = resultados.pose_landmarks.landmark
+
+                        if inicio_video(landmarks,mp_pose):
+                            bandera_rodilla = True
+                        
+                        if camara == "T":
+                            if fin_video_coordenadas(camara,landmarks,mp_pose,bandera_rodilla):
+                                bandera_fin = True 
+                        elif camara == "LI" or camara == "LD":
+                            if fin_video_coordenadas(camara,landmarks,mp_pose,bandera_rodilla):
+                                bandera_fin = True 
+                           
+
+                        if bandera_rodilla == False:
+                            num_frame_inicio += 1
+                            mp_marcar.draw_landmarks(
+                                frame,
+                                resultados.pose_landmarks,
+                                mp_pose.POSE_CONNECTIONS,
+                                mp_marcar.DrawingSpec(color=(0, 0, 255), thickness=1, circle_radius=1),
+                                mp_marcar.DrawingSpec(color=(255, 255, 255), thickness=1, circle_radius=1)
+                            )
+                        else:
+                            if bandera_fin == False: 
+                                mp_marcar.draw_landmarks(
+                                    frame,
+                                    resultados.pose_landmarks,
+                                    mp_pose.POSE_CONNECTIONS,
+                                    mp_marcar.DrawingSpec(color=(255, 0, 0), thickness=1, circle_radius=3),
+                                    mp_marcar.DrawingSpec(color=(255, 255, 255), thickness=1, circle_radius=1)
+                                )
+                            else:
+                                mp_marcar.draw_landmarks(
+                                    frame,
+                                    resultados.pose_landmarks,
+                                    mp_pose.POSE_CONNECTIONS,
+                                    mp_marcar.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=3),
+                                    mp_marcar.DrawingSpec(color=(255, 255, 255), thickness=1, circle_radius=1)
+                                )
+
+                    cv2.imshow("Mediapipe Pose", frame)
+
+            key = cv2.waitKey(25) & 0xFF
+            if key == ord('q'): 
+                break
+            elif key == ord('p'):  # Pausar
+                pausa = not pausa
+
+    captura.release()
+    cv2.destroyAllWindows()
+    return(path," - ",num_frame_inicio)
+
+
+print(lectura_video("C:/Users/laura/OneDrive - Pontificia Universidad Javeriana/Videos Tesis/Saques de Piso/LateralDerecha (Lau)/Piso_LD_4.MOV"))
+print(lectura_video("C:/Users/laura/OneDrive - Pontificia Universidad Javeriana/Videos Tesis/Saques de Piso/LateralIzquierda (Sofi)/Piso_LI_4.MOV"))
+print(lectura_video("C:/Users/laura/OneDrive - Pontificia Universidad Javeriana/Videos Tesis/Saques de Piso/Trasera(Andy)/Piso_T_4.MOV"))
+
