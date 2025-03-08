8b19a44684df404c170cda6f4a02421d6bece527
LauraSSB <laurasalamanca1135@gmail.com>
2025-03-08 10:47:59 -0500

    cambio general

--- Codigo/renombrar_videos.py
+++ Codigo/renombrar_videos.py
@@ -1,6 +1,6 @@
 import os
 
-path = "G:/Mi unidad/Videos Trabajo Grado/Trasera (Andy)"
+path = "G:/Mi unidad/Videos Trabajo de Grado/Lateral Derecha (Lau)"
 
 archivos = sorted(os.listdir(path))
 
@@ -9,8 +9,8 @@ for i, nombre_archivo in enumerate(archivos, start=1):
     
     file_extension = os.path.splitext(nombre_archivo)[1]
     
-    nombre_nuevo = f"Piso_LI_{i}{file_extension}"
-    path_nuevo = os.path.join(path, new_name)
+    nombre_nuevo = f"Piso_LD_{i}{file_extension}"
+    path_nuevo = os.path.join(path, nombre_nuevo)
 
     try:
         os.rename(path_ant, path_nuevo)
@@ -18,4 +18,4 @@ for i, nombre_archivo in enumerate(archivos, start=1):
     except Exception as e:
         print(f"Error al renombrar: {nombre_archivo} (Índice: {i})")
 
-print("Renombrado completado (con errores indicados si los hubo).")
+print("Finalizo")
